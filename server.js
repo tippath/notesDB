28b7561af8cfc29171473ab05b439f26ef056055
tippath <110766269+tippath@users.noreply.github.com>
2022-09-27 06:07:55 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { networkInterfaces } = require("os");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+mongoose.connect("mongodb+srv://test:test@cluster0.npe8jxd.mongodb.net/notesDB", { useNewUrlParser: true }, { useUnifiedTopology: true })
+
+// crwate a data schema
+const notesSchema = {
+    title: String,
+    content: String
+}
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req,res){
+    let newNode = new Note({
+        title: req.body.title,
+        content: req.body.content
+    });
+    newNode.save();
+    res.redirect('/');
+})
+
+
+app.listen(3000, function() {
+    console.log("server is running on 3000");
+})
